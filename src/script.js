77a14524bdcd38b24fa5565156d3634f2671eae3
Tony <320164921+tonysharrk@users.noreply.github.com>
2026-08-24 20:52:39 +0000

    Create gameboard & cell logic js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,45 @@
-console.log("ho67");
+import "./styles.css"
+
+function gameBoard(rows, columns) {
+  const board= []; //non-primitive
+
+  // Create the gameboard
+  for (let i=0; i< rows; i++) {
+    board.push([]);
+    for (let j= 0; j< columns; j++) {
+      board[i].push(cell());
+    }
+  }
+
+  // Function for getting the gameboard with only the cell values (Getter for the board)
+  const getGameBoard= () => {
+    return board.map((row) => {
+      return row.map((cell) => {
+        return cell.getCellValue();
+      })
+    })
+  }
+  
+  return {getGameBoard};
+}
+
+//Get & Setter for specific cells
+function cell() {
+  let cellValue= "_"; //primitive
+
+  const getCellValue= () => {
+    return cellValue;
+  };
+
+  const setCellValue= (value) => {
+    cellValue = value;
+  };
+
+  return {getCellValue, setCellValue};
+}
+
+
+const board= gameBoard(3,3);
+console.log(board.getGameBoard());
+
+// function gamecontroller
